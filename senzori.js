a8e2eb7901b382ca7fb70958944457019cf65b9a
danipetrascu <dani.ovidiu30@gmail.com>
2017-11-20 18:34:32 +0200

    Math operations

--- senzori.js
+++ senzori.js
@@ -5,19 +5,19 @@ window.addEventListener("deviceorientation,on_device_orientation);
 //------------------------------------
 function on_device_orientation(e)
 {
-	document.getElementByID("id_z").innerHTML = "z="+e.alpha;
-	document.getElementByID("id_x").innerHTML = "x="+e.beta;
-	document.getElementByID("id_y").innerHTML = "y="+e.gamma;
+	document.getElementByID("id_z").innerHTML = "z="+ Math.round(e.alpha * 100)/100;
+	document.getElementByID("id_y").innerHTML = "y="+ Math.round(e.gamma * 100)/100;
+	document.getElementByID("id_x").innerHTML = "x="+ Math.round(e.beta * 100)/100;
 }
 //------------------------------------
 function on_device_motion(e)
 {
-	document.getElementByID("id_acc_x").innerHTML = "acc_x" + e.acceleration.x;
-	document.getElementByID("id_acc_y").innerHTML = "acc_y" + e.acceleration.y;
-	document.getElementByID("id_acc_z").innerHTML = "acc_z" + e.acceleration.z;
+	document.getElementByID("id_acc_x").innerHTML = "acc_x" + Math.round(e.acceleration.x * 100)/100;
+	document.getElementByID("id_acc_y").innerHTML = "acc_y" + Math.round(e.acceleration.y * 100)/100;
+	document.getElementByID("id_acc_z").innerHTML = "acc_z" + Math.round(e.acceleration.z * 100)/100;
 	
-	document.getElementByID("id_acc_g_x").innerHTML = "acc_g_x" + e.accelerationIncludingGravity.x;
-	document.getElementByID("id_acc_g_y").innerHTML = "acc_g_y" + e.accelerationIncludingGravity.y;
-	document.getElementByID("id_acc_g_z").innerHTML = "acc_g_z" + e.accelerationIncludingGravity.z;
+	document.getElementByID("id_acc_g_x").innerHTML = "acc_g_x" + Math.round(e.accelerationIncludingGravity.x * 100)/100;
+	document.getElementByID("id_acc_g_y").innerHTML = "acc_g_y" + Math.round(e.accelerationIncludingGravity.y * 100)/100;
+	document.getElementByID("id_acc_g_z").innerHTML = "acc_g_z" + Math.round(e.accelerationIncludingGravity.z * 100)/100;
 }
 //------------------------------------	
