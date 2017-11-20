165e24010f2ed76c2c096c994d6811db408f0a8e
danipetrascu <dani.ovidiu30@gmail.com>
2017-11-20 19:09:27 +0200

    Update3

--- Daniel.js
+++ Daniel.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.20.1";
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.20.2";
 window.addEventListener("deviceorientation",on_device_orientation);
 window.addEventListener("devicemotion",on_device_motion);
 function on_device_orientation(e)
@@ -19,7 +19,7 @@ function on_device_motion(e)
 	document.getElementById("id_acc_g_z").innerHTML="acc g z =" + Math.round(e.accelerationIncludingGravity.z * 100)/100;
 
 	var beta = Math.atan(e.accelerationIncludingGravity.x / e.accelerationIncludingGravity.z) * 180 / Math.PI;
-	var gamma = Math.atan(e.accelerationIncludingGravity.y / e.accelerationIncludingGravity.z) * 180 / Math.PI;
+	var gamma = - Math.atan(e.accelerationIncludingGravity.y / e.accelerationIncludingGravity.z) * 180 / Math.PI;
 
 	document.getElementById("id_rot_x").innerHTML="beta=" + Math.round(beta * 100)/100;
 	document.getElementById("id_rot_y").innerHTML="gamma=" + Math.round(gamma * 100)/100;
