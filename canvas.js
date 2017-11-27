7d74d76fcb5fae7a6a8881e46e04aea9c78524db
danipetrascu <dani.ovidiu30@gmail.com>
2017-11-27 18:23:15 +0200

    2017-11-27 Canvas - culoare linie cerc
    Colorarea contului cercului cu o anumita culoare si de o anumita grosime

--- canvas.js
+++ canvas.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.27.4";
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.27.5";
 
 var canvas = document.getElementById("id_canvas");
 var ctx = canvas.getContext("2d");
@@ -6,7 +6,9 @@ var ctx = canvas.getContext("2d");
 ctx.beginPath();
 ctx.arc(100,100,20,0,2 * Math.PI);
 //"TRUE" se refera la sensul acelor de ceas sau sensul contrar acelor de ceas
-ctx.fillStyle = "#FF0000"
+ctx.fillStyle = "#FF0000";
 //"#FF0000" = #-culoare , FF-ROSU, 00-VERDE, 00-ALBASTRU 
 ctx.fill();
+ctx.strokeStyle = "#00FF00";
+ctx.lineWidth = 5;
 ctx.stroke();
