4a48cd46463691781fc4f2668c5127a9c0b25b9f
danipetrascu <dani.ovidiu30@gmail.com>
2017-11-27 18:20:35 +0200

    2017-11-27 Canvas - cerc colorat
    Umplerea unui cerc desenat cu o anumita culoare

--- canvas.js
+++ canvas.js
@@ -1,9 +1,12 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.27.2";
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.27.4";
 
 var canvas = document.getElementById("id_canvas");
 var ctx = canvas.getContext("2d");
 
 ctx.beginPath();
-ctx.arc(100,100,20,0,Math.PI/2,true);
+ctx.arc(100,100,20,0,2 * Math.PI);
 //"TRUE" se refera la sensul acelor de ceas sau sensul contrar acelor de ceas
+ctx.fillStyle = "#FF0000"
+//"#FF0000" = #-culoare , FF-ROSU, 00-VERDE, 00-ALBASTRU 
+ctx.fill();
 ctx.stroke();
