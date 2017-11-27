68c454493603516fec20935509b211164fac9a30
danipetrascu <dani.ovidiu30@gmail.com>
2017-11-27 18:55:00 +0200

    2017-11-27 Canvas - senzori_canvas
    Desenare bila pe telefon, coreland pozitia in care tinem telefonu.

---
+++ senzori_canvas.js
@@ -0,0 +1,23 @@
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.20.0";
+
+window.addEventListener("deviceorientation",on_device_orientation);
+//-----------------------------------------------------------------------------------------------------------------------
+function deseneaza_cerc (unghi1, unghi2)
+{
+	var canvas = document.getElementById("id_canvas");
+	var ctx = canvas.getContext("2d");
+
+	ctx.beginPath();
+	ctx.arc(200+unghi1*200/90,200+unghi2*200/90,20,0,2 * Math.PI);		//"0,0" locul de unde sa inceapa desenu; "20" raza cercului
+	ctx.fillStyle = "#FF0000";			//"#FF0000" = #-culoare , FF-ROSU, 00-VERDE, 00-ALBASTRU 
+	ctx.fill();
+	ctx.strokeStyle = "#00FF00";		//Culoarea data reprezinta culoarea contului cercului
+	ctx.lineWidth = 5;					//Numarul "5" reprezinta grosimea contului
+	ctx.stroke();
+}
+//-----------------------------------------------------------------------------------------------------------------------
+function on_device_orientation(e)
+{
+	deseneaza_cerc(e.beta, e.gamma);
+}
+//----------------------------------------------------------------------------------------------------------------------
