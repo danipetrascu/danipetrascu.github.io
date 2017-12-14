05dcd76b57b35020317960a34b76bed88c04daa6
danipetrascu <dani.ovidiu30@gmail.com>
2017-12-14 19:10:55 +0200

    Geolocation

---
+++ gps.js
@@ -0,0 +1,16 @@
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.0";
+
+navigator.geolocation.getCurrentPosition();
+//-------------------------------------------
+function on_position_success(e)
+{
+	document.getElementById("id_lat").innerHTML = e.coords.latitude;
+    document.getElementById("id_long").innerHTML = e.coords.longitude;
+}
+
+ //------------------------------------------
+function on_position_failure(e)
+{
+	alert("M-am pierdut");
+}
+//-------------------------------------------
