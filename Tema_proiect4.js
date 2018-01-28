ddd876ab08b5e17e3968f4d220090667ca59ac4d
danipetrascu <33325724+danipetrascu@users.noreply.github.com>
2018-01-28 15:02:14 +0200

    Tema4_v1
    Varianta 1 a temei 4

---
+++ Tema_proiect4.js
@@ -0,0 +1,17 @@
+window.addEventListener("device orientation",on_device_orientation);
+window.addEventListener("device motion",on_device_motion);
+function on_device_orientation(e)
+{
+	document.getElementById("id_z").innerHTML ="z " + e.alpha;
+	document.getElementById("id_y").innerHTML ="y " + e.gamma;
+	document.getElementById("id_x").innerHTML ="x " + e.beta;
+}
+
+function on_device_motion(e)
+{
+	document.getElementById("id_acc_x").innerHTML="acc x =" + e.acceleration.x;
+	document.getElementById("id_acc_y").innerHTML="acc y ="e.acceleration.y;
+	document.getElementById("id_acc_z").innerHTML="acc z ="e.acceleration.z;
+	document.getElementById("id_acc_g_x").innerHTML= "acc g x ="e.accelerationIncludingGravity.x;
+	document.getElementById("id_acc_g_y").innerHTML= "acc g y ="e.accelerationIncludingGravity.y;
+	document.getElementById("id_acc_g_z").innerHTML= "acc g z ="e.accelerationIncludingGravity.z;
